829598d5e3b45e170ef9b4432e0d04091ed1992d
Evgenii <Evgenii@gmail.com>
2023-07-02 23:35:48 +0500

    sprint_14.0.8

--- app.js
+++ app.js
@@ -8,6 +8,7 @@ const routeSignin = require('./routes/sign-in');
 const routeUsers = require('./routes/users');
 const routeCards = require('./routes/cards');
 const auth = require('./middlewares/auth');
+const errorHandler = require('./middlewares/errorHandler');
 const NotFoundError = require('./errors/NotFoundError');
 
 const { PORT = 3000 } = process.env;
@@ -26,6 +27,7 @@ app.use('/users', routeUsers);
 app.use('/cards', routeCards);
 app.use((req, res, next) => next(new NotFoundError('Запрашиваемый ресурс не найден.')));
 app.use(errors());
+app.use(errorHandler);
 
 mongoose
   .connect('mongodb://127.0.0.1:27017/mestod')
