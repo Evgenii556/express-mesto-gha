9fbe67baeb896485b5474a8edfa840e601fad714
Evgenii <Evgenii@gmail.com>
2023-07-04 00:31:53 +0500

    sprint_14.1.1

--- models/user.js
+++ models/user.js
@@ -69,11 +69,9 @@ const userSchema = new Schema(
             if (user) {
               return bcrypt.compare(password, user.password).then((matched) => {
                 if (matched) return user;
-
                 return new AuthError('Некорректные почта или пароль');
               });
             }
-
             return new AuthError('Некорректные почта или пароль');
           });
       },
